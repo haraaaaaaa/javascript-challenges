a93d2c0f21470dde7184bf9175370f0d7a56fcdf
haraaaaaaa <harunhebibovic05@gmail.com>
2023-02-25 19:16:44 +0100

    finished task

--- onclickevent_practice/app.js
+++ onclickevent_practice/app.js
@@ -1,7 +1,34 @@
-const crveniPassport = document.getElementById("container-text").getElementsByClassName("crveniPassport")
-const cener = document.getElementById("container-text").getElementsByClassName("cener")
-const kriminal = document.getElementById("container-text").getElementsByClassName("kriminal")
+// Text elements
+const crveniPassport = document.querySelector(".crveniPassport");
+const kriminal = document.querySelector(".kriminal");
+const cener = document.querySelector(".cener");
 
-const crveniPassportVideo = document.getElementById("container-video").getElementsByClassName("crveniPassportVideo")
-const cenerVideo = document.getElementById("container-video").getElementsByClassName("cenerVideo")
-const kriminalVideo = document.getElementById("container-video").getElementsByClassName("kriminalVideo")
+// Video elements
+const crveniPassportVideo = document.querySelector(".crveniPassportVideo");
+const kriminalVideo = document.querySelector(".kriminalVideo");
+const cenerVideo = document.querySelector(".cenerVideo");
+
+function clearScreen(){
+    crveniPassportVideo.style.visibility = "hidden";
+    cenerVideo.style.visibility = "hidden";
+    kriminalVideo.style.visibility = "hidden";
+}
+
+function showCrveniPassport(){
+    clearScreen();
+    crveniPassportVideo.style.visibility = "visible";
+}
+
+function showCener(){
+    clearScreen();
+    cenerVideo.style.visibility = "visible";
+}
+
+function showKriminal(){
+    clearScreen();
+    kriminalVideo.style.visibility = "visible";
+}
+
+crveniPassport.addEventListener('click', showCrveniPassport)
+kriminal.addEventListener('click', showKriminal)
+cener.addEventListener('click', showCener)
