eb5ec55f34d0a7e64a313e8c456bf4bdc903c98d
haraaaaaaa <harunhebibovic05@gmail.com>
2023-02-25 18:48:20 +0100

    added some variables in .js

--- onclickevent_practice/app.js
+++ onclickevent_practice/app.js
@@ -1,3 +1,7 @@
 const crveniPassport = document.getElementById("container-text").getElementsByClassName("crveniPassport")
 const cener = document.getElementById("container-text").getElementsByClassName("cener")
 const kriminal = document.getElementById("container-text").getElementsByClassName("kriminal")
+
+const crveniPassportVideo = document.getElementById("container-video").getElementsByClassName("crveniPassportVideo")
+const cenerVideo = document.getElementById("container-video").getElementsByClassName("cenerVideo")
+const kriminalVideo = document.getElementById("container-video").getElementsByClassName("kriminalVideo")
