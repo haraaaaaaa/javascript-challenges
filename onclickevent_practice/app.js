2c2d3277e88842b9cee65f642dc5cad4361c97a6
haraaaaaaa <harunhebibovic05@gmail.com>
2023-02-25 18:31:21 +0100

    finished aligning the fucking text

---
+++ onclickevent_practice/app.js
@@ -0,0 +1,7 @@
+const crveniPassport = document.querySelector(".crveniPassport")
+const cener = document.querySelector(".cener")
+const kriminal = document.querySelector(".kriminal")
+
+crveniPassport.addEventListener("click", function(){
+    
+})
